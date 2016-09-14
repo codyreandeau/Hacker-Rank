6a525080b76a5e4e61c75c8234c44d39fb0d029e
Cody Reandeau <codyreandeau@yahoo.com>
2016-09-14 12:56:33 -0400

    Solution to printing out the elements in a linked list, and inserting a node at the end of a linked list

---
+++ Data Structures/Linked Lists/Insert a node at the tail of a linked list.java
@@ -0,0 +1,26 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+// This is a "method-only" submission. 
+// You only need to complete this method.
+  if(head == null) {
+    head = new Node();
+    head.data = data;
+    }else{
+      Node newNode = head;
+      while(newNode.next != null){
+          newNode = newNode.next;
+      }
+      newNode.next = new Node();
+      newNode.next.data = data;
+  }
+    return head;
+}
+}
